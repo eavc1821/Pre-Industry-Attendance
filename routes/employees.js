6e100c470225a52275846a680a2217406f57de56
Alejandro Carcamo <eavc1821@gmail.com>
2025-11-17 13:05:14 -0600

    allQuery add

--- routes/employees.js
+++ routes/employees.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const qr = require("qr-image");
-const { runQuery, getQuery } = require("../config/database");
+const { runQuery, getQuery, allQuery } = require("../config/database");
 const { authenticateToken, requireAdminOrScanner } = require("../middleware/auth");
 const cloudinary = require("../config/cloudinary");
 const upload = require("../config/multerCloudinary");
